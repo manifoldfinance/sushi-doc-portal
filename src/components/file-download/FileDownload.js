464ddd08dcdd48513a3ab1e07b45f30a35108f74
sam bacha <sam@manifoldfinance.com>
2022-02-19 00:44:50 -0800

    feat: initial docs (#2)

--- src/components/file-download/FileDownload.js
+++ src/components/file-download/FileDownload.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import styles from './FileDownload.module.css';
-import FileDownloadIcon from '../../../static/img/file-icon.svg'
+import FileDownloadIcon from '../../../static/img/file-icon.svg';
 
 export class FileDownload extends React.Component {
-    render() {
-        const {
-            link,
-            title
-        } = this.props;
+  render() {
+    const { link, title } = this.props;
 
-        return <a className={styles.fileDownload}
-                  href={link}
-                  target="_blank">
-            <FileDownloadIcon className={styles.fileDownloadIcon}/>
-            <p className={styles.fileDownloadTitle}>{title}</p>
-        </a>;
-    }
+    return (
+      <a className={styles.fileDownload} href={link} target="_blank">
+        <FileDownloadIcon className={styles.fileDownloadIcon} />
+        <p className={styles.fileDownloadTitle}>{title}</p>
+      </a>
+    );
+  }
 }
--- src/components/homepage-footer/HomepageFooter.js
+++ src/components/homepage-footer/HomepageFooter.js
@@ -1,6 +1,6 @@
 import ChevronRightImg from '../../../static/img/chevron-right.svg';
 import DiscordImage from '../../../static/img/icons/discord.svg';
-import FooterLogoImage from '../../../static/img/footer-logo.svg'
+import FooterLogoImage from '../../../static/img/footer-logo.svg';
 import GithubImage from '../../../static/img/icons/github.svg';
 import Link from '@docusaurus/Link';
 import MediumImage from '../../../static/img/icons/medium.svg';
@@ -13,147 +13,158 @@ import clsx from 'clsx';
 import styles from './HomepageFooter.module.css';
 
 const FooterLinks = {
-    links: [
+  links: [
+    {
+      title: 'Protocols',
+      links: [
         {
-            title: 'Protocols',
-            links: [
-                {
-                    title: 'Sushi Liquidity',
-                    link: '#'
-                },
-                {
-                    title: 'Bentobox Staking',
-                    link: '#'
-                },
-                {
-                    title: 'Kashi Lending',
-                    link: '#'
-                }
-            ]
+          title: 'Sushi Liquidity',
+          link: '#',
         },
         {
-            title: 'Networks',
-            links: [
-                {
-                    title: 'Ethereum',
-                    link: '#'
-                },
-                {
-                    title: 'Avalanche',
-                    link: '#'
-                },
-                {
-                    title: 'Polygon',
-                    link: '#'
-                }
-            ]
+          title: 'Bentobox Staking',
+          link: '#',
         },
         {
-            title: 'Resources',
-            links: [
-                {
-                    title: 'Forums',
-                    link: '#'
-                },
-                {
-                    title: 'Support',
-                    link: '#'
-                },
-                {
-                    title: 'Governance',
-                    link: '#'
-                },
-                {
-                    title: 'Contact',
-                    link: '#'
-                },
-                {
-                    title: 'Legal',
-                    link: '#'
-                }
-            ]
+          title: 'Kashi Lending',
+          link: '#',
         },
-
-    ],
-    socialMedia: [
+      ],
+    },
+    {
+      title: 'Networks',
+      links: [
         {
-            href: '#',
-            image: <MediumImage/>
+          title: 'Ethereum',
+          link: '#',
         },
         {
-            href: '#',
-            image: <RedditImage/>
+          title: 'Avalanche',
+          link: '#',
         },
         {
-            href: '#',
-            image: <TwitterImage/>
+          title: 'Polygon',
+          link: '#',
+        },
+      ],
+    },
+    {
+      title: 'Resources',
+      links: [
+        {
+          title: 'Forums',
+          link: '#',
         },
         {
-            href: '#',
-            image: <DiscordImage/>
+          title: 'Support',
+          link: '#',
         },
         {
-            href: '#',
-            image: <YoutubeImage/>
+          title: 'Governance',
+          link: '#',
         },
         {
-            href: '#',
-            image: <TelegramImage/>
+          title: 'Contact',
+          link: '#',
         },
         {
-            href: '#',
-            image: <GithubImage/>
-        }
-    ]
-}
+          title: 'Legal',
+          link: '#',
+        },
+      ],
+    },
+  ],
+  socialMedia: [
+    {
+      href: '#',
+      image: <MediumImage />,
+    },
+    {
+      href: '#',
+      image: <RedditImage />,
+    },
+    {
+      href: '#',
+      image: <TwitterImage />,
+    },
+    {
+      href: '#',
+      image: <DiscordImage />,
+    },
+    {
+      href: '#',
+      image: <YoutubeImage />,
+    },
+    {
+      href: '#',
+      image: <TelegramImage />,
+    },
+    {
+      href: '#',
+      image: <GithubImage />,
+    },
+  ],
+};
 
 export default function HomepageFooter() {
-    return (
-        <section className={styles.footerTopBorder}>
-            <div className={clsx('page-container', styles.footer)}>
-                <div className={styles.footerColumnWrap}>
-                    {
-                        FooterLinks.links.map((props, idx) => (
-                            <div className={styles.footerColumn} key={idx}>
-                                <span className={styles.footerColumnTitle}>{props.title}</span>
-                                {
-                                    props.links.map((linkProps, index) => (
-                                        <Link key={index} className={styles.footerColumnLink} to={linkProps.link}>
-                                            {linkProps.title}
-                                        </Link>
-                                    ))
-                                }
-                            </div>
-                        ))
-                    }
-                </div>
-                <div>
-                    <p className={styles.footerSubscribeTitle}>Omakase Report</p>
-                    <p className={styles.footerSubscribeText}>Listings, events, and more</p>
-                    <Link to='#'
-                          className={styles.footerSubscribeButton}>
-                        <span className={styles.footerSubscribeButtonText}>Subscribe</span>
-                        <ChevronRightImg/>
-                    </Link>
-                    <div className={styles.socialMediaWrap}>
-                        {
-                            FooterLinks.socialMedia.map((media, idx) => (
-                                <Link className={styles.socialMediaLink} to={media.href} key={idx}>
-                                    {media.image}
-                                </Link>
-                            ))
-                        }
-                    </div>
-                </div>
+  return (
+    <section className={styles.footerTopBorder}>
+      <div className={clsx('page-container', styles.footer)}>
+        <div className={styles.footerColumnWrap}>
+          {FooterLinks.links.map((props, idx) => (
+            <div className={styles.footerColumn} key={idx}>
+              <span className={styles.footerColumnTitle}>{props.title}</span>
+              {props.links.map((linkProps, index) => (
+                <Link
+                  key={index}
+                  className={styles.footerColumnLink}
+                  to={linkProps.link}
+                >
+                  {linkProps.title}
+                </Link>
+              ))}
             </div>
-            <div className={clsx('page-container', styles.afterFooter)}>
-                <div className={styles.footerLogoContainer}>
-                    <FooterLogoImage className={styles.footerLogo}/>
-                    <span>&#169; {new Date().getFullYear()} Sushiswap Community - CC4.0 </span>
-                </div>
-                <a href="https://etherscan.io/address/0x11Ca5375AdAfd6205E41131A4409f182677996E6#readContract" target="_blank">BoringHelperV2</a>
-                <a href="" target="_blank">Made with 🍣 Tsukiji Fish Market</a>
-            </div>
-        </section>
-    );
+          ))}
+        </div>
+        <div>
+          <p className={styles.footerSubscribeTitle}>Omakase Report</p>
+          <p className={styles.footerSubscribeText}>
+            Listings, events, and more
+          </p>
+          <Link to="#" className={styles.footerSubscribeButton}>
+            <span className={styles.footerSubscribeButtonText}>Subscribe</span>
+            <ChevronRightImg />
+          </Link>
+          <div className={styles.socialMediaWrap}>
+            {FooterLinks.socialMedia.map((media, idx) => (
+              <Link
+                className={styles.socialMediaLink}
+                to={media.href}
+                key={idx}
+              >
+                {media.image}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={clsx('page-container', styles.afterFooter)}>
+        <div className={styles.footerLogoContainer}>
+          <FooterLogoImage className={styles.footerLogo} />
+          <span>
+            &#169; {new Date().getFullYear()} Sushiswap Community - CC4.0{' '}
+          </span>
+        </div>
+        <a
+          href="https://etherscan.io/address/0x11Ca5375AdAfd6205E41131A4409f182677996E6#readContract"
+          target="_blank"
+        >
+          BoringHelperV2
+        </a>
+        <a href="" target="_blank">
+          Made with 🍣 Tsukiji Fish Market
+        </a>
+      </div>
+    </section>
+  );
 }
